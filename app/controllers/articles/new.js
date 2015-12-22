977dac35b8c2f32d2583d96a4ced992540c2ce99
Rushaine McBean <rushaine.mcbean@gmail.com>
2015-12-22 14:18:55 -0500

    Adds a nested article index & creation of new articles

---
+++ app/controllers/articles/new.js
@@ -0,0 +1,11 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+	isValid: Ember.computed(
+		'model.description'
+		{
+			get() {
+				return !Ember.isEmpty(this.get('model.description'));
+		}
+	)
+});
