3e0ffe0d3340e83278f7d1c044518b5fdcd082e8
Rushaine McBean <rushaine.mcbean@gmail.com>
2015-12-22 12:52:17 -0500

    Create article resource

--- app/models/friend.js
+++ app/models/friend.js
@@ -3,6 +3,7 @@ import Ember from 'ember';
 
 
 export default DS.Model.extend({
+  articles: DS.hasMany('article'),
   firstName: DS.attr('string'),
   lastName: DS.attr('string'),
   email: DS.attr('string'),
