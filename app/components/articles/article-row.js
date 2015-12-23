292536b225640b48036de64ecefb93f5509f3504
Rushaine McBean <rushaine.mcbean@gmail.com>
2015-12-23 13:15:09 -0500

    Changes the article to save each the state is changed

--- app/components/articles/article-row.js
+++ app/components/articles/article-row.js
@@ -1,4 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+	tagName: 'tr',
+	article: null,
+	articleStates: null,
+	actions: {
+		saveArticle(article) {
+			let article = this.get('article');
+
+			if (article.get('hasDirtyAttributes')) {
+				this.sendAction('save', article)
+			}
+		}
+	}
 });
