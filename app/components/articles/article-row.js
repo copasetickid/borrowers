552c32742bcb8fd5f2caec86892bcacd504deb7b
Rushaine McBean <rushaine.mcbean@gmail.com>
2015-12-23 14:18:01 -0500

    Adds in route hooks to remove unsaved records from the client store

--- app/components/articles/article-row.js
+++ app/components/articles/article-row.js
@@ -5,7 +5,7 @@ export default Ember.Component.extend({
 	article: null,
 	articleStates: null,
 	actions: {
-		saveArticle(article) {
+		saveArticle() {
 			let article = this.get('article');
 
 			if (article.get('hasDirtyAttributes')) {
--- app/controllers/articles/new.js
+++ app/controllers/articles/new.js
@@ -1,11 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-	isValid: Ember.computed(
-		'model.description'
-		{
-			get() {
-				return !Ember.isEmpty(this.get('model.description'));
-		}
+	hasDescription: Ember.computed.notEmpty('model.description'),
+	isValid: Ember.computed.and(
+		'hasDescription'
 	)
 });
